31ef4c3d8e28c6b129fa02d398fdc3e266ff5576
Kshiraja Nelapati <181626461+kshirajahere@users.noreply.github.com>
2026-02-27 12:37:08 +0530

    Update selection-sort.c

--- selection-sort.c
+++ selection-sort.c
@@ -4,17 +4,20 @@
 void selectionSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
           int min_idx=i;
-          for(int j=i+1;i<n;i++){
-                 if(arr[j]<arr[min_idx]) min_idx=i;
+          for(int j=i+1;j<n;j++){
+                 if(arr[j]<arr[min_idx]) 
+                      min_idx=j;
+         }
           int temp=arr[min_idx];
           arr[min_idx]=arr[i];
           arr[i]=temp;
-          }
     }
 }
 int main(){
     int arr[]={64,25,92,22,11};
     selectionSort(arr,5);
-    for(int i=0;i<5;i++)printf("%d\t",arr[i]);
+    for(int i=0;i<5;i++){
+    printf("%d\t",arr[i]);
+    }
     return 0;
 }
